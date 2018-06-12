89a268cfa4e65f56f36e443de1975ac773e92082
englederlucas <lucasengleder@gmail.com>
2018-06-12 18:00:58 +0200

    package.json

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ const parser = require('body-parser')
 
 //mongoose.connect('mongodb+srv://engllucas:' + res + '@insight-quhku.mongodb.net/test');
 
+
+
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
+
+
+
 let db = mongoose.connection;
 
 
